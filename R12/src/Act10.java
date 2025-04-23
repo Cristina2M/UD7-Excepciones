eed156a89a5baba521c3753c76df72534d939726
Cristina2M <cmormar2509@g.educaand.es>
2025-04-23 12:30:01 +0000

    10,11

---
+++ R12/src/Act10.java
@@ -0,0 +1,30 @@
+public class Act10 {
+    public static void main(String[] args) {
+    try {
+        System.out.println(metodo());
+    } catch (Exception e) {
+        System.out.println("Excepción en método() ");
+        e.printStackTrace();
+    }
+    }
+
+    public static int metodo(){
+    int valor=0;
+    try {
+        valor = valor + 1;
+        valor = valor + Integer.parseInt("42");
+        valor = valor + 1;
+        System.out.println("Valor al final del try: " + valor);
+    } catch (NumberFormatException e) {
+        valor = valor + Integer.parseInt("42");
+        System.out.println("Valor al final del catch: " + valor);
+    } finally {
+        valor = valor + 1;
+        System.out.println("Valor al final del finally: " + valor);
+    }
+        valor = valor + 1;
+        System.out.println("Valor antes del return: " + valor);
+        return valor;
+    }
+}
+
---
+++ R12/src/Act11.java
@@ -0,0 +1,34 @@
+public class Act11 {
+    public static void main(String[] args) {
+    try {
+        System.out.println(metodo());
+    } catch (Exception e) {
+        System.out.println("Excepción en método() ");
+        e.printStackTrace();
+    }
+    }
+
+    public static int metodo()throws NumberFormatException{
+    int valor=0;
+    try {
+        valor = valor + 1;
+        valor = valor + Integer.parseInt("W");
+        valor = valor + 1;
+        System.out.println("Valor al final del try: " + valor);
+    } catch (NumberFormatException e) {
+        valor = valor + Integer.parseInt("42");
+        System.out.println("Valor al final del catch: " + valor);
+        throw e;
+    } finally {
+        valor = valor + 1;
+        System.out.println("Valor al final del finally: " + valor);
+    }
+        valor = valor + 1;
+        System.out.println("Valor antes del return: " + valor);
+        return valor;
+    }
+}
+
+
+
+
